778ddae960dee49a7ab6d6462357bdbcbcbbd51b
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-11 22:15:37 +0300

    Update Fire HUB.lua

--- Fire HUB.lua
+++ Fire HUB.lua
@@ -1,1 +1,4 @@
-
+local id = game.PlaceId
+if id == 6516141723 or id == 6839171747 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-Hub/main/DOORS👁.lua"))()
+end
