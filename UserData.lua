b0961fb6ed1d69f8d62b4ad7bec34054a7d7fbb3
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-11 21:46:30 +0300

    Create UserData.lua

---
+++ UserData.lua
@@ -0,0 +1,6 @@
+yes="whitelisted"no="blacklisted"local owner = "6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"local hwidWhitelist={
+    ["6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"] = yes,
+}local vip = {
+    ["6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"] = yes,
+}
+return hwidWhitelist,vip,owner
