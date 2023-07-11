acab752d4917a3c73f039e255dde7fc006c95b62
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-12 00:29:28 +0300

    Update Fire HUB.lua

--- Fire HUB.lua
+++ Fire HUB.lua
@@ -1,4 +1,10 @@
-local id = game.PlaceId
+warn("FIRE~HUB: EARLY LOADING PROCESS HAS BEGUN...")
+warn("Getting place info...")
+local id = game.PlaceId local name = game:GetService("MarketplaceService"):GetProductInfo(id).Name
+warn("Searching script for game: "..tostring(id).." ("..name..")...")
+function executeScript(gameName)gameName = tostring(gameName).."lua" warn("Found script for game: "..tostring(id).." ("..name..") - "..gameName)warn("Executing "..gameName.."...")loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-Hub/main/"..gameName))()warn(gameName.." has been successfuly executed!")end
 if id == 6516141723 or id == 6839171747 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-Hub/main/DOORS.lua"))()
+    executeScript("DOORS")return
 end
+warn("No scripts found for game: "..tostring(id))
+return
