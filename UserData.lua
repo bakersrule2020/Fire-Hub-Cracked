c789a4967af1ae3cd0539171dcb77f167ff83a82
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-12 00:34:48 +0300

    Update UserData.lua

--- UserData.lua
+++ UserData.lua
@@ -1,7 +1,11 @@
-yes="whitelisted"no="blacklisted"local owner = "6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"local hwidWhitelist={
+local yes="whitelisted"
+local no="blacklisted"
+local owner = "6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"
+local hwidWhitelist = {
     ["6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"] = yes,
     ["57a436ab07d5fa7cc1047b8ac141857919afa65845d9ba57dd59cb34ead961a67dbd5cfeef99fd4c9abe758c3b5fdbdb"] = yes,
-}local vip = {
+}
+local vip = {
     ["6ccf60fb0725008729753881acc7667fbdb2801d51c0ba394119c4af5d17fa47"] = yes,
 }
 return hwidWhitelist,vip,owner
