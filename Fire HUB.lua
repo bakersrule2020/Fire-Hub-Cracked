9b0e71e90e3954524ba5faf216c90a7691dd56b1
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-12 00:53:11 +0300

    Update Fire HUB.lua

--- Fire HUB.lua
+++ Fire HUB.lua
@@ -2,7 +2,15 @@ warn("FIRE~HUB: EARLY LOADING PROCESS HAS BEGUN...")
 warn("Getting place info...")
 local id = game.PlaceId local name = game:GetService("MarketplaceService"):GetProductInfo(id).Name
 warn("Searching script for game: "..tostring(id).." ("..name..")...")
-function executeScript(gameName)warn("Converting ["..gameName.."] to ["..gameName..".lua]...") gameName = tostring(gameName)..".lua" warn("Found script for game: "..tostring(id).." ("..name..") - "..gameName)warn("Executing "..gameName.."...")loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-Hub/main/"..gameName))()warn(gameName.." has been successfuly executed!")end
+function executeScript(gameName)
+    warn("Found script for game: "..tostring(id).." ("..name..") - "..gameName)
+    warn("Converting ["..gameName.."] to ["..gameName..".lua]...")
+    gameName = tostring(gameName)..".lua"
+    warn("Converted!")
+    warn("Executing "..gameName.."...")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-Hub/main/"..gameName))()
+    warn(gameName.." has been successfuly executed!")
+end
 if id == 6516141723 or id == 6839171747 then
     executeScript("DOORS")return
 end
