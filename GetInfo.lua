70bc28537788a8e461307a9d4304217f0f6fe3d0
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-12 12:31:44 +0300

    Update GetInfo.lua

--- GetInfo.lua
+++ GetInfo.lua
@@ -5,17 +5,17 @@ pcall(function()
 		task.spawn(function()
 			local http_request = syn and syn.request or request
 			local body, decoded
-			if http_request and not hwid then
+			if http_request then
 				body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body
 				decoded = game:GetService('HttpService'):JSONDecode(body)
 				for i, v in pairs(decoded.headers) do
 					if string.match(string.lower(i), 'fingerprint') then hwid = v break end
 				end
-			elseif not http_request and not hwid then
+			elseif not http_request and hwid == "" then
 				cant = true
 			end
 		end)
 	end)()
 end)
-repeat task.wait(0) until hwid or cant
+repeat task.wait(0) until hwid ~= "" or cant
 return hwid
