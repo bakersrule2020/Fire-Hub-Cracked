4e7e029c887cb7dd26369f70107646a495708b35
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-12 12:32:52 +0300

    Update GetInfo.lua

--- GetInfo.lua
+++ GetInfo.lua
@@ -1,20 +1,20 @@
 local cant = false
 local hwid = ""
 pcall(function()
-	coroutine.wrap(function()
-		task.spawn(function()
-			local http_request = syn and syn.request or request
-			local body, decoded
-			if http_request then
-				body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body
-				decoded = game:GetService('HttpService'):JSONDecode(body)
-				for i, v in pairs(decoded.headers) do
-					if string.match(string.lower(i), 'fingerprint') then hwid = v break end
-				end
-			elseif not http_request and hwid == "" then
-				cant = true
-			end
-		end)
+    coroutine.wrap(function()
+        task.spawn(function()
+            local http_request = syn and syn.request or request
+            local body, decoded
+            if http_request then
+                body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body
+                decoded = game:GetService('HttpService'):JSONDecode(body)
+                for i, v in pairs(decoded.headers) do
+                    if string.match(string.lower(i), 'fingerprint') then hwid = v break end
+                end
+            else
+                cant = true
+            end
+    	end)
 	end)()
 end)
 repeat task.wait(0) until hwid ~= "" or cant
