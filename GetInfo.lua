2b128a5bd307532e123dfb55f244a2b21c263398
InfernusScripts <139264639+InfernusScripts@users.noreply.github.com>
2023-07-12 12:26:53 +0300

    Update GetInfo.lua

--- GetInfo.lua
+++ GetInfo.lua
@@ -1,21 +1,25 @@
 local cant = false
-coroutine.wrap(function()
-	task.spawn(function()
-		local http_request = syn and syn.request or request
-		local body, decoded
-		if http_request and not _G.hwid then
-			body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body
-			decoded = game:GetService('HttpService'):JSONDecode(body)
-			for i, v in pairs(decoded.headers) do
-				if string.match(string.lower(i), 'fingerprint') then _G.hwid = v end
+
+pcall(function()
+	coroutine.wrap(function()
+		task.spawn(function()
+			local http_request = syn and syn.request or request
+			local body, decoded
+			if http_request and not _G.hwid then
+				body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body
+				decoded = game:GetService('HttpService'):JSONDecode(body)
+				for i, v in pairs(decoded.headers) do
+					if string.match(string.lower(i), 'fingerprint') then _G.hwid = v break end
+				end
+			elseif not http_request and not _G.hwid then
+				cant = true
 			end
-		elseif not http_request and not _G.hwid then
-			cant = true
-		end
-		if _G.hwid then
-			cant = false
-		end
-	end)
-end)()
+			if _G.hwid then
+				cant = false
+			end
+		end)
+	end)()
+end)
 repeat task.wait(0) until _G.hwid or cant
-print(cant, _G.hwid)
+_G.hwid = _G.hwid or ""
+return _G.hwid
