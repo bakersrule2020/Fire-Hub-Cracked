5d39abaaca9c01daafa20fbafd72e1e399e4c785
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-08-02 22:31:56 +0300

    Update UserData.lua

--- UserData.lua
+++ UserData.lua
@@ -1,7 +1,10 @@
 local whitelist = {
   ["823e87f5b06a3fb8d2249e715a1d3535"] = "whitelisted", --titanzxd
   ["92aba5b46a07a91970141be86464c96a"] = "whitelisted", --titanzxd's brother
+  ["fa600df48f8b8c2a9704e2089e6046d5"] = "whitelisted", --Yumos
 }
-local vip = {}
+local vip = {
+  ["fa600df48f8b8c2a9704e2089e6046d5"] = "whitelisted", --Yumos
+}
 local owner = ""
 return whitelist,vip,owner
