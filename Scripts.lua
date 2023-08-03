055b40fec921910d72eb68320e8098635c40b5c2
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-08-03 03:15:44 +0300

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,8 +1,5 @@
 _G.distance = 15
 _G.MaxBullets = math.huge
-_G.Range = 10000000
-_G.OnAnything = true
-_G.Fail = false
 return {
   ["Infinite yield"] = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",
   ["Dex Explorer"] = "https://raw.githubusercontent.com/infyiff/backup/main/dex.lua",
@@ -10,5 +7,4 @@ return {
   ["Synapse Compatibility Script"] = "https://raw.githubusercontent.com/Tamim468/Valyseonly/main/synsupport.lua",
   ["Gravity Gun [FE on some parts]"] = "https://pastebin.com/raw/9PKRvvVt",
   ["M249"] = "https://pastebin.com/raw/yb5nUq6v",
-  ["Crucifix on Everything"] = "https://raw.githubusercontent.com/PenguinManiack/Crucifix/main/Crucifix.lua",
 }
