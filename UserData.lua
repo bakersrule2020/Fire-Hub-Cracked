664235f067dbe3ba4ceace97fd8c06ae53e7b687
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-08-04 19:35:45 +0300

    Update UserData.lua

--- UserData.lua
+++ UserData.lua
@@ -6,6 +6,7 @@ local whitelist = {
   ["fa600df48f8b8c2a9704e2089e6046d5"] = "конечно", --Yumos
   ["eda0fb4c371062ca45b4e78ce3bf2515"] = "true", --Evanboi420
   ["6b001bf6540004eac9a4b2cb3a26bf46"] = true, --тоха долбаёб
+  ["0c3ba589c12afdd998e476426d9dee3b"] = Instance.new("Part"),
 }
 local vip = {
   ["91f1e364c09e0e2b42b43bcd78d76f79"] = true, --INFERNUS
