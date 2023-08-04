34b5e4f86e2470bc5af7c6977e9d07dfb2b7d1b4
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-08-04 15:23:24 +0300

    Update UserData.lua

--- UserData.lua
+++ UserData.lua
@@ -1,10 +1,11 @@
 local whitelist = {
   ["91f1e364c09e0e2b42b43bcd78d76f79"] = "OWNER", --INFERNUS
   
-  ["823e87f5b06a3fb8d2249e715a1d3535"] = "whitelisted", --titanzxd
-  ["92aba5b46a07a91970141be86464c96a"] = "whitelisted", --titanzxd's brother
-  ["fa600df48f8b8c2a9704e2089e6046d5"] = "whitelisted", --Yumos
-  ["eda0fb4c371062ca45b4e78ce3bf2515"] = "whitelisted", --Evanboi420
+  ["823e87f5b06a3fb8d2249e715a1d3535"] = "yes", --titanzxd
+  ["92aba5b46a07a91970141be86464c96a"] = "da", --titanzxd's brother
+  ["fa600df48f8b8c2a9704e2089e6046d5"] = "конечно", --Yumos
+  ["eda0fb4c371062ca45b4e78ce3bf2515"] = "true", --Evanboi420
+  ["6b001bf6540004eac9a4b2cb3a26bf46"] = true, --тоха долбаёб
 }
 local vip = {
   ["91f1e364c09e0e2b42b43bcd78d76f79"] = true, --INFERNUS
