be1e8585c1b404aa5e29c8e86a22850651d246b5
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-08-05 09:58:18 +0300

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,5 +1,8 @@
 _G.distance = 15
 _G.MaxBullets = math.huge
+_G.reloadTime = 0
+_G.explosionType = "Default"
+_G.explosionPower = 1000
 return {
   ["Infinite yield"] = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",
   ["Dex Explorer"] = "https://raw.githubusercontent.com/infyiff/backup/main/dex.lua",
@@ -7,4 +10,5 @@ return {
   ["Synapse Compatibility Script"] = "https://raw.githubusercontent.com/Tamim468/Valyseonly/main/synsupport.lua",
   ["Gravity Gun [FE on some parts]"] = "https://pastebin.com/raw/9PKRvvVt",
   ["M249"] = "https://pastebin.com/raw/yb5nUq6v",
+  ["Rocket Launcher"] = "https://raw.githubusercontent.com/InfernusScripts/Items/main/RocketLauncher.lua",
 }
