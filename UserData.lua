47fb55f3e7d57375ca4c738373e413f29d487083
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-08-15 15:33:04 +0300

    Update UserData.lua

--- UserData.lua
+++ UserData.lua
@@ -5,24 +5,26 @@ PASTE FREAKING
 ]]--
 
 local whitelist = {
-  ["91f1e364c09e0e2b42b43bcd78d76f79"] = "OWNER", --INFERNUS
-  
-  ["823e87f5b06a3fb8d2249e715a1d3535"] = "yes", --titanzxd
-  ["92aba5b46a07a91970141be86464c96a"] = "da", --titanzxd's brother
-  ["fa600df48f8b8c2a9704e2089e6046d5"] = "конечно", --Yumos
-  ["eda0fb4c371062ca45b4e78ce3bf2515"] = "true", --Evanboi420
-  ["6b001bf6540004eac9a4b2cb3a26bf46"] = true, --тоха долбаёб
-  ["0c3ba589c12afdd998e476426d9dee3b"] = game, --wvpul
-  ["4a8ccb6cb27a70d81164436034a78197"] = "game:Shutdown()", --eshaanopn
-  ["a73485e263e1b1d95fc43d820c36486c"] = table, --idk
-  ["a8281b8c14a1d5bb30a47d9c3ae8cdb2"] = "да я люблю сосать член", --банановая кожурка
-  ["4716a3e01184ddc86cf494f8f56b6bbd"] = "ohhhh, sempai, deeper!", --пон
-  ["685393549a6cbd0817444827040e785c"] = "miNEcrAAAAAAAAAAAAAAAAAAAAAAAAAAft", --the piston
-  ["685393549a6cbd0817444827040e785c"] = "a", --kevin
-  ["f201eac56fd543297a94b4de13e06a9f"] = "агент слоник", -- :)
-  ["d7d9219563275f7b5cb4411046c92dce"] = "no", --quagmire
-  ["45c8dcf06cbeb0c8d6a45768a8876776"] = "mega beatbox SKVRRRRR VR VR ptt vRRRvr VRVvr VR", --squirell
-  ["de74320031298b39b9e42e23966f5d02"] = 'this is different';
+	["91f1e364c09e0e2b42b43bcd78d76f79"] = "OWNER", --INFERNUS
+
+	["823e87f5b06a3fb8d2249e715a1d3535"] = "yes", --titanzxd
+	["92aba5b46a07a91970141be86464c96a"] = "da", --titanzxd's brother
+	["fa600df48f8b8c2a9704e2089e6046d5"] = "конечно", --Yumos
+	["eda0fb4c371062ca45b4e78ce3bf2515"] = "true", --Evanboi420
+	["6b001bf6540004eac9a4b2cb3a26bf46"] = true, --тоха долбаёб
+	["0c3ba589c12afdd998e476426d9dee3b"] = game, --wvpul
+	["4a8ccb6cb27a70d81164436034a78197"] = "game:Shutdown()", --eshaanopn
+	["a73485e263e1b1d95fc43d820c36486c"] = table, --idk
+	["a8281b8c14a1d5bb30a47d9c3ae8cdb2"] = "да я люблю сосать член", --банановая кожурка
+	["4716a3e01184ddc86cf494f8f56b6bbd"] = "ohhhh, sempai, deeper!", --пон
+	["685393549a6cbd0817444827040e785c"] = "miNEcrAAAAAAAAAAAAAAAAAAAAAAAAAAft", --the piston
+	["f201eac56fd543297a94b4de13e06a9f"] = "агент слоник", -- :)
+	["d7d9219563275f7b5cb4411046c92dce"] = "no", --quagmire
+	["45c8dcf06cbeb0c8d6a45768a8876776"] = "mega beatbox SKVRRRRR VR VR ptt vRRRvr VRVvr VR", --squirell
+	["de74320031298b39b9e42e23966f5d02"] = 'this is different';
+	["519040ebefabffbbea74ad3e786bad63"] = '""', --3EloHIyChay123123
+	["9497460701cd126be3e49ad0ebee30bd"] = "''", --bryld
+	["d446c5f1fcf3cb7a4614dc8365ef8588"] = type, --penguinl
 }
 local vip = {
   ["91f1e364c09e0e2b42b43bcd78d76f79"] = "OWNER", --INFERNUS
